ffeb8caa52d110375b21c29d84a00d85d6efb0ad
DinushanSriskandaraja <sinushansriskandaraja@gmail.com>
2025-05-06 18:35:29 +0530

    User-Backend

---
+++ BackEnd/apps/product/models.py
@@ -0,0 +1,15 @@
+# myapp/models.py
+from django.db import models
+from .models import user  # Import the custom User model
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    farmer = models.ForeignKey(user, on_delete=models.CASCADE, related_name='products', limit_choices_to={'role': 'farmer'})
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} (by {self.farmer.name})"
